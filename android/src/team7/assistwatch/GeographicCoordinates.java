d7721da32e63d2b97099c53563b896699ba0c4f0
Thomas Pickham <thomas.pickham@connect.qut.edu.au>
2014-08-14 17:32:53 +1000

    Initial commit

---
+++ android/src/team7/assistwatch/GeographicCoordinates.java
@@ -0,0 +1,12 @@
+package team7.assistwatch;
+
+public class GeographicCoordinates {
+	
+	public double latitude;
+	public double longitude;
+	
+	public GeographicCoordinates() {
+		latitude = 0.0;
+		longitude = 0.0;
+	}
+}
