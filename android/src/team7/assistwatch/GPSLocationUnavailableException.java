1a5595b0a8b998348926bdffbd9210056f050622
Vox <newvoxel@gmail.com>
2014-08-28 15:02:48 +1000

    Complete redo android project

--- android/src/team7/assistwatch/GPSLocationUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package team7.assistwatch;
-
-public class GPSLocationUnavailableException extends Exception {
-    public GPSLocationUnavailableException(String message) {
-        super(message);
-    }
-}
