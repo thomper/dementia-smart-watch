f969c6121653c2ecb542b2dcc9161dca01be6029
TashPotatoes <natasha.bethany.moffat@gmail.com>
2014-09-08 19:27:06 -0700

    Circles functionality

--- web/scripts/maps.js
+++ web/scripts/maps.js
@@ -1,7 +1,8 @@
 //Function to retrieve map
-function initialize(patientLat, patientLong, patientName, patientStatus) {
+function initialize(patientLat, patientLong, patientName, patientStatus, fenceRadius, fenceLat, fenceLng) {
 	var map_canvas = document.getElementById('mapcanvas');
 	var LatLng = new google.maps.LatLng(patientLat, patientLong);
+	var FenceLatLng = new google.maps.LatLng(fenceLat, fenceLong);
 	//Create string for marker window
 	var patientString = '<div id="mapcontent">' +
       '<div id="siteNotice">' +
@@ -19,12 +20,28 @@ function initialize(patientLat, patientLong, patientName, patientStatus) {
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
 	
+	//Circle Details
+	var fenceOptions = {
+	      strokeColor: '#FF0000',
+	      strokeOpacity: 0.8,
+	      strokeWeight: 2,
+	      fillColor: '#FF0000',
+	      fillOpacity: 0.35,
+	      map: map,
+	      center: FenceLatLng;
+	      radius: fenceRadius;
+	    };
+	    
+	//Creates a circle
+	var radialFence = new google.maps.Circle(fenceOptions);
+
 	//Creates map
 	var map = new google.maps.Map(map_canvas, map_options)
-	
+
 	var patientInfowindow = new google.maps.InfoWindow({
 	content: patientString});
 
+	//Creates a marker
 	var patientMarker = new google.maps.Marker({
 	position: LatLng,
 	map: map,
