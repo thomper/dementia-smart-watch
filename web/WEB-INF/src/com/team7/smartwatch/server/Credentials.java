375f41a02cc2806b8589f98b40f80c766a1cbb3b
Thomas Pickham <thomas.pickham@connect.qut.edu.au>
2014-09-18 03:44:36 +1000

    Refactor LoginServlet

---
+++ web/WEB-INF/src/com/team7/smartwatch/server/Credentials.java
@@ -0,0 +1,6 @@
+package com.team7.smartwatch.server;
+
+public class Credentials {
+	public String username;
+	public String password;
+}
--- web/WEB-INF/src/com/team7/smartwatch/server/LoginServlet.java
+++ web/WEB-INF/src/com/team7/smartwatch/server/LoginServlet.java
@@ -20,140 +20,134 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
 public class LoginServlet extends HttpServlet {
 
 	private static final long serialVersionUID = -4701061750449836620L;
-	private static final Logger logger =
-			Logger.getLogger(LoginServlet.class.getName());
+	private static final Logger logger = Logger.getLogger(LoginServlet.class
+			.getName());
 	private static final String USERS_TABLE = "users";
-    private static final String SELECT_STATEMENT =
-    		"SELECT userPass, salt FROM " + USERS_TABLE +
-    		" WHERE userName = ?";
-    private static final String SUCCESS_MESSAGE = "Login successful";
-    private static final String ERROR_MESSAGE = "Authentication failed";
+	private static final String SELECT_STATEMENT = "SELECT userPass, salt FROM "
+			+ USERS_TABLE + " WHERE userName = ?";
+	private static final String SUCCESS_MESSAGE = "Login successful";
+	private static final String ERROR_MESSAGE = "Authentication failed";
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-    		throws IOException, ServletException
-    {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        boolean succeeded = authenticate(request);
-        if (succeeded) {
-            out.println(SUCCESS_MESSAGE);
-        } else {
-            out.println(ERROR_MESSAGE);
-        }
-    }
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		boolean succeeded = authenticate(request);
+		if (succeeded) {
+			out.println(SUCCESS_MESSAGE);
+		} else {
+			out.println(ERROR_MESSAGE);
+		}
+	}
 
-    private boolean authenticate(HttpServletRequest request) {
-        Connection conn = null;
-        String requestUsername = null;
-        String requestPassword = null;
-        String storedHash = null;
-        String salt = null;
-        
-        try {
-            conn = DatabaseConnector.getConnection();
-            PreparedStatement selectUser = conn.prepareStatement(SELECT_STATEMENT);
-            
-            // As we can't pass strings by reference, we use temporary arrays
-            // instead.
-            String[] arrayUsername = {""};
-            String[] arrayPassword = {""};
-            getRequestCredentials(request, arrayUsername, arrayPassword);
-            requestUsername = arrayUsername[0];
-            requestPassword = arrayPassword[0];
+	private boolean authenticate(HttpServletRequest request) {
+		// TODO: Refactor! Extract database and business logic code.
+		Connection conn = null;
+		Credentials credentials = new Credentials();
+		String storedHash = null;
+		String salt = null;
 
-            bindValues(selectUser, requestUsername);
-            ResultSet resultSet = selectUser.executeQuery();
-            if (resultSet.next()) {
-            	storedHash = resultSet.getString("userPass");
-            	salt = resultSet.getString("salt");
-            } else {
-				logger.log(Level.INFO, "Failed login attempt from "
-						+ request.getRemoteAddr() + " for nonexistent username "
-						+ requestUsername + ".");
-            	return false;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (BadPostParameterException e) {
-            // TODO: log error?
+		// Read password hash from database associated with given username.
+		try {
+			conn = DatabaseConnector.getConnection();
+			PreparedStatement selectUser = conn
+					.prepareStatement(SELECT_STATEMENT);
+			getRequestCredentials(request, credentials);
+			bindValues(selectUser, credentials.username);
+			ResultSet resultSet = selectUser.executeQuery();
+			if (resultSet.next()) {
+				storedHash = resultSet.getString("userPass");
+				salt = resultSet.getString("salt");
+			} else {
+				logger.log(Level.INFO,
+						"Failed login attempt from " + request.getRemoteAddr()
+								+ " for nonexistent username "
+								+ credentials.username + ".");
+				return false;
+			}
+		} catch (SQLException e) {
+			// TODO
+			e.printStackTrace();
+		} catch (BadPostParameterException e) {
+			logger.log(Level.INFO, "Bad post parameter given in login " + 
+					"attempt from " + request.getRemoteAddr() + ".");
 		} finally {
-			
+
 			// Close database connection.
-            try {
-                if (conn != null) {
-                    conn.close();
-                    conn = null;
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        
-        if(Utility.arrayContainsNull(requestPassword, storedHash, salt)) {
-        	logger.log(Level.INFO, "Error accessing user info from database.");
-        	return false;
-        }
-        
-        boolean correct = passwordIsCorrect(requestPassword, storedHash, salt);
-        if (correct) {
+			try {
+				if (conn != null) {
+					conn.close();
+					conn = null;
+				}
+			} catch (SQLException e) {
+				// TODO
+				e.printStackTrace();
+			}
+		}
+
+		if (Utility.arrayContainsNull(credentials.password, storedHash, salt)) {
+			logger.log(Level.INFO, "Error accessing user info from database.");
+			return false;
+		}
+
+		boolean correct = passwordIsCorrect(credentials.password, salt,
+				storedHash);
+		if (correct) {
 			logger.log(Level.INFO,
 					"Successful login from " + request.getRemoteAddr()
-							+ " for user " + requestUsername + ".");
-        } else {
+							+ " for user " + credentials.username + ".");
+		} else {
 			logger.log(Level.INFO,
 					"Failed login attempt from " + request.getRemoteAddr()
-							+ " for user " + requestUsername + ".");
-        }
+							+ " for user " + credentials.username + ".");
+		}
 
-        return correct;
-    }
-    
+		return correct;
+	}
+
 	private boolean passwordIsCorrect(String requestPassword,
-			String storedHash, String salt) {
+			String salt, String storedHash) {
 
-		String requestHash = null;
-		
 		try {
 			String passwordHash = Utility.SHA256(requestPassword);
-			requestHash = Utility.SHA256(passwordHash + salt);
+			String requestHash = Utility.SHA256(passwordHash + salt);
+			return requestHash.equals(storedHash);
 		} catch (NoSuchAlgorithmException e) {
+			// TODO
 			e.printStackTrace();
 			return false;
 		}
-		
-		return requestHash.equals(storedHash);
-    }
+	}
 
-    private void bindValues(PreparedStatement selectUser, String username)
-            throws BadPostParameterException {
-    	
-        try {
-            // TODO: validate username
-            selectUser.setString(1, username);
-        } catch (SQLException e) {
-        	// TODO: log
-            e.printStackTrace();
-        } catch (JSONException e) {
-        	throw new BadPostParameterException();
-        }
-    }
-    
+	/* Place given username in prepared statement. */
+	private void bindValues(PreparedStatement selectUser, String username)
+			throws BadPostParameterException {
+
+		try {
+			selectUser.setString(1, username);
+		} catch (SQLException e) {
+			// TODO
+			e.printStackTrace();
+		} catch (JSONException e) {
+			throw new BadPostParameterException();
+		}
+	}
+
+	/* Get the username and password from the HTTP request. */
 	private void getRequestCredentials(HttpServletRequest request,
-			String[] username, String[] password) throws BadPostParameterException {
-    	
-        	JSONObject jObj;
-			try {
-				jObj = JSONConverter.getJSON(request);
-				username[0] = jObj.getString("username");
-				password[0] = jObj.getString("password");
-			} catch (IOException | JSONException e) {
-				e.printStackTrace();
-				throw new BadPostParameterException();
-			}
-    }
+			Credentials credentials) throws BadPostParameterException {
+
+		JSONObject jObj;
+		try {
+			jObj = JSONConverter.getJSON(request);
+			credentials.username = jObj.getString("username");
+			credentials.password = jObj.getString("password");
+		} catch (IOException | JSONException e) {
+			throw new BadPostParameterException();
+		}
+	}
 }
