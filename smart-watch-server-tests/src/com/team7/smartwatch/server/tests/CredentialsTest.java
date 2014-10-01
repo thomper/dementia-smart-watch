35e136e900e424e90616d5b0e6e76fd02ed986c7
SStirling93 <S.Stirling@live.com.au>
2014-10-01 15:18:21 +1000

    CredentialsTest Update
    Added a JUnit Test for CredentialsTest

--- smart-watch-server-tests/src/com/team7/smartwatch/server/tests/CredentialsTest.java
+++ smart-watch-server-tests/src/com/team7/smartwatch/server/tests/CredentialsTest.java
@@ -8,12 +8,20 @@ import org.junit.Test;
 
 public class CredentialsTest {
 
+	String storedHash = "d31ca2b572992e2ff5335a3a6ada16f64e1f1b7f3999462f8058819b9a7da075";
+	String salt = "b0973ae4-1640-4c4e-b350-d38d10bb865a";
+	
 	@Test
 	public void testPasswordIsCorrect_MatchingHash_true() {
 		Credentials credentials = new Credentials();
 		credentials.password = "password";
-		String storedHash = "d31ca2b572992e2ff5335a3a6ada16f64e1f1b7f3999462f8058819b9a7da075";
-		String salt = "b0973ae4-1640-4c4e-b350-d38d10bb865a";
 		assertTrue(credentials.passwordIsCorrect(salt, storedHash));
+	}
+	
+	@Test
+	public void testPasswordIsCorrect_MatchingHash_false() {
+		Credentials credentials = new Credentials();
+		credentials.password = "passwordwrong";
+		assertFalse(credentials.passwordIsCorrect(salt, storedHash));
 	}
 }
