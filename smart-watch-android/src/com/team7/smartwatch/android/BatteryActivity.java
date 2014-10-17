edf6236d290bfdab62659e2bc8e429959b48b63c
Thomas Pickham <thomas.pickham@connect.qut.edu.au>
2014-10-18 07:28:27 +1000

    Reformat and clean up BatteryActivity

--- smart-watch-android/src/com/team7/smartwatch/android/BatteryActivity.java
+++ smart-watch-android/src/com/team7/smartwatch/android/BatteryActivity.java
@@ -1,76 +1,54 @@
 package com.team7.smartwatch.android;
 
- 
-import android.app.Activity;  
-import android.content.BroadcastReceiver;  
-import android.content.Context;  
-import android.content.Intent;  
-import android.content.IntentFilter;  
-import android.os.BatteryManager;  
-import android.os.Bundle;  
-import android.util.Log;  
-import android.view.View;  
-import android.view.View.OnClickListener;  
-import android.widget.TextView;  
- 
- public class BatteryActivity extends Activity {
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
-      TextView mTextView;  
-      float mBatteryLevel; 
-      BatteryReceiver mArrow;  
+public class BatteryActivity extends Activity {
 
-      @Override  
-      protected void onCreate(Bundle savedInstanceState) {  
-           super.onCreate(savedInstanceState);  
-           setContentView(R.layout.activity_battery);  
-           mTextView = (TextView) findViewById(R.id.battery_status);  
-           findViewById(R.id.battery_getbutton).setOnClickListener(new OnClickListener() {  
-                @Override  
-                public void onClick(View v) {  
-                     mTextView.setText(String.valueOf(mBatteryLevel));  
-                }  
-           });  
-           new Thread(new ThreadM()).start();  
-      }  
+	TextView mTextView;
+	float mBatteryLevel;
 
-      @Override  
-      protected void onDestroy() {  
-           super.onDestroy();  
-           unregisterReceiver(mArrow);  
-      }  
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-      private class ThreadM implements Runnable {  
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_battery);
+		mTextView = (TextView) findViewById(R.id.battery_status);
+		findViewById(R.id.battery_getbutton).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						mTextView.setText(String.valueOf(mBatteryLevel));
+					}
+				});
+		new Thread(new ThreadM()).start();
+	}
 
-           @Override  
-           public void run() {  
-                mArrow = new BatteryReceiver();  
-                IntentFilter intentFilter = new IntentFilter();  
-                intentFilter.addAction(Intent.ACTION_BATTERY_LOW);  
-                intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);  
-                intentFilter.addAction(Intent.ACTION_BATTERY_OKAY);  
-                Intent batteryIntent = registerReceiver(mArrow, intentFilter);  
-                mBatteryLevel = getBatteryLevel(batteryIntent);  
-                Log.e("Battery Level", String.valueOf(mBatteryLevel));  
-           }  
-      }  
-      
-      
-      private class BatteryReceiver extends BroadcastReceiver {  
-          @Override  
-          public void onReceive(Context arg0, Intent arg1) {  
-               
-          }  
-     }   
-      
-      public float getBatteryLevel(Intent batteryIntent) {  
-           int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);  
-           int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);  
-           if (level == -1 || scale == -1) {  
-                return 50.0f;  
-           }  
-           return ((float) level / (float) scale) * 100.0f;  
-      }  
-      
+	private class ThreadM implements Runnable {
 
- }  
- 
+		@Override
+		public void run() {
+			mBatteryLevel = getBatteryLevel();
+			Log.e("Battery Level", String.valueOf(mBatteryLevel));
+		}
+	}
+
+	public float getBatteryLevel() {
+
+		Intent batteryIntent = registerReceiver(null, new IntentFilter(
+				Intent.ACTION_BATTERY_CHANGED));
+		int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+		int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+		if (level == -1 || scale == -1) {
+			return -1;
+		}
+		return ((float) level / (float) scale) * 100.0f;
+	}
+}
